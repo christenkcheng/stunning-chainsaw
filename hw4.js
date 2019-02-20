dee0f4744775e405e7be134cb00ce204e31d338e
christenkcheng <47799530+christenkcheng@users.noreply.github.com>
2019-02-19 16:47:18 -0800

    Update hw4.js

--- hw4.js
+++ hw4.js
@@ -35,14 +35,17 @@ function getCookie(cookie_name) {
 function loadDoc(url, myfunction) {
 	let xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status == 200) {
-			myfunction(this);
+		if (this.readyState === 4 && this.status === 200) {
+			document.getElementById("testing").innerHTML = this.responseText;
+			//myfunction(this);
 		}
 	};
 	xhttp.open("GET",url,true);
 	xhttp.send();
 }
 
+/*
 function myfunction(xhttp) {
 	document.getElementById("testing").innerHTML = xhttp.responseText;
 }
+*/
